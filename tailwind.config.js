c47c7b75faaa7e096a56f532fd3dce2e57ee74f8
Yaxyobek <pubgn9642@gmail.com>
2025-06-16 21:44:39 +0500

    Update dependencies: Upgrade React and React DOM to version 19.1.0, add Hero UI packages, and include Framer Motion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
+const {heroui} = require('@heroui/theme');
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js"
+  ],
   theme: {
     extend: {
       colors: {
@@ -155,5 +160,5 @@ export default {
       "2xl": "1536px",
     },
   },
-  plugins: [],
+  plugins: [heroui()],
 };
