5f988c0b65dcb74cad9a3e6c1ac0c974547a3845
yaxyobekuz <pubgn9642@gmail.com>
2025-06-23 11:59:34 +0500

    Refactor Logo component to use a div instead of h1 for better semantic structure; update scroll-to-top styles for improved visibility and transition effects.

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <h1 className="text-2xl font-semibold">
+    <div className="text-2xl font-semibold">
       <Link to="/" title="TestNova asosiy sahifasi">
         Test<span className="text-primary-500">Nova</span>
       </Link>
-    </h1>
+    </div>
   );
 };
 
