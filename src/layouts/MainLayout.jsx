352ebaec277b038ff7055c3bd89b4145ccbe2262
yaxyobekuz <pubgn9642@gmail.com>
2025-06-23 11:47:16 +0500

    Add scroll to top efature to main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,23 @@
+import { useEffect } from "react";
+
 // Router
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+
+// Components
+import ScrollToTopButton from "../components/common/ScrollToTopButton";
 
 const MainLayout = () => {
+  const location = useLocation();
+  useEffect(() => window.scrollTo(0, 0), [location.pathname]);
+
   return (
     <div className="flex flex-col min-h-screen">
       {/* main */}
       <main className="">
         <Outlet />
       </main>
+
+      <ScrollToTopButton />
     </div>
   );
 };
