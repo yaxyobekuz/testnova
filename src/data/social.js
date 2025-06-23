0ed402a0d190787be1f4473f81b48b6fcb23f110
yaxyobekuz <pubgn9642@gmail.com>
2025-06-23 09:23:21 +0500

    Add social media data with icons and descriptions

---
+++ src/data/social.js
@@ -0,0 +1,26 @@
+import youtubeIcon from "../assets/icons/multicolor/youtube.svg";
+import telegramIcon from "../assets/icons/multicolor/telegram.svg";
+import instagramIcon from "../assets/icons/multicolor/instagram.svg";
+
+const social = [
+  {
+    name: "Telegram",
+    icon: telegramIcon,
+    url: "https://telegram.me/testnovauz",
+    description: "TestNova telegram kanali",
+  },
+  {
+    name: "Instagram",
+    icon: instagramIcon,
+    url: "https://instagram.com/",
+    description: "TestNova instagram sahifasi",
+  },
+  {
+    name: "Youtube",
+    icon: youtubeIcon,
+    url: "https://youtube.com/",
+    description: "TestNova youtube sahifasi",
+  },
+];
+
+export default social;
