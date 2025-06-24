b5a0b01b7ec121978bd4af07272adb6f881be0bf
yaxyobekuz <pubgn9642@gmail.com>
2025-06-24 12:38:52 +0500

    Add CategoryItem responsive styles

--- src/components/common/CategoryItem.jsx
+++ src/components/common/CategoryItem.jsx
@@ -9,27 +9,29 @@ import arrowTopRightIcon from "../../assets/icons/outline/arrow-top-right-primar
 const CategoryItem = ({ name, description, icon, background, link }) => {
   return (
     <li
-      className={`${background} flex items-center gap-3.5 relative p-5 rounded-2xl group`}
+      className={`${background} flex items-center gap-3.5 relative p-3.5 rounded-2xl group sm:p-5`}
     >
       {/* Icon */}
       <Icon
         size={56}
         src={icon}
         alt={name}
-        className="size-14 bg-neutral-50 rounded-lg"
+        className="size-14 bg-neutral-50 rounded-full sm:rounded-lg"
       />
 
       {/* Info */}
       <div>
-        <h3 className="text-lg font-semibold">{name}</h3>
-        <p className="text-neutral-500 mt-1.5">{description}</p>
+        <h3 className="font-semibold sm:text-lg">{name}</h3>
+        <p className="text-neutral-500 mt-1.5 text-sm xs:text-base">
+          {description}
+        </p>
       </div>
 
       {/* Top right icon */}
       <Icon
         alt="Yuqoriga o'ngga"
         src={arrowTopRightIcon}
-        className="absolute top-5 right-5 opacity-0 translate-y-1 -translate-x-1 transition-all duration-200 group-hover:opacity-100 group-hover:translate-y-0 group-hover:translate-x-0"
+        className="absolute top-3.5 right-3.5 opacity-0 translate-y-1 -translate-x-1 transition-all duration-200 group-hover:opacity-100 group-hover:translate-y-0 group-hover:translate-x-0 sm:top-5 sm:right-5"
       />
 
       {/* Link */}
