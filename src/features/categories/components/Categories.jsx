61f2003546a46d17a63ba46fe1d9fb9bb2540677
yaxyobekuz <pubgn9642@gmail.com>
2025-06-25 19:28:28 +0500

    Add categories page responsive styles

--- src/features/categories/components/Categories.jsx
+++ src/features/categories/components/Categories.jsx
@@ -6,13 +6,13 @@ import CategoryItem from "../../../components/common/CategoryItem";
 
 const Categories = () => {
   return (
-    <div className="pt-10 pb-20 bg-gradient-to-b from-transparent to-white">
+    <div className="pt-10 pb-16 bg-gradient-to-b from-transparent to-white md:pb-20">
       <div className="container">
         {/* Title */}
         <h1 className="mb-8">Bo'limlar</h1>
 
         {/* List */}
-        <ul className="grid grid-cols-2 gap-5 mb-8">
+        <ul className="grid grid-cols-1 gap-5 md:grid-cols-2">
           {categories.map((category, index) => (
             <CategoryItem key={index} {...category} />
           ))}
