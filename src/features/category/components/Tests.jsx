13e3219c60d5e695657b4dba4651233d64887ebd
yaxyobekuz <pubgn9642@gmail.com>
2025-07-03 21:33:56 +0500

    Refactor Tests component to load test data from API and display loading skeleton

--- src/features/category/components/Tests.jsx
+++ src/features/category/components/Tests.jsx
@@ -1,19 +1,66 @@
+import { useEffect } from "react";
+
+// Redux (Store)
+import {
+  setTestsLoading,
+  updateTestsFromStore,
+} from "../../../store/features/testsSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+// Components
+import { useParams } from "react-router-dom";
+import testsService from "../../../api/services/testsService";
+
 // Components
 import TestItem from "../../../components/common/TestItem";
+import TestItemSkeleton from "../../../components/skeleton/TestItem";
 
 const Tests = () => {
+  const dispatch = useDispatch();
+  const { categoryLink } = useParams();
+  const { data, isLoading } = useSelector((state) => state.tests);
+
+  const loadTests = () => {
+    testsService
+      .getTests(categoryLink)
+      .then(({ data }) => {
+        dispatch(updateTestsFromStore({ name: categoryLink, data }));
+      })
+      .finally(() => dispatch(setTestsLoading(false)));
+  };
+
+  useEffect(() => {
+    if (!data[categoryLink]?.length) loadTests();
+  }, []);
+
+  // Loading
+  if (isLoading) return <TestsSkeleton />;
+
+  // Content
   return (
     <div className="py-8 md:py-10">
       <div className="container">
         <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-          {Array.from({ length: 12 }, (_, index) => (
+          {data[categoryLink]?.map((test, index) => (
             <TestItem
-              count={10}
+              {...test}
               key={index}
-              title="HTML basic"
-              description={"HTML bo'yicha boshlang'ich savollar"}
               icon="https://andarov-frontquiz.netlify.app/img/js.svg"
             />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const TestsSkeleton = () => {
+  return (
+    <div className="py-8 md:py-10">
+      <div className="container">
+        <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <TestItemSkeleton key={index} />
           ))}
         </ul>
       </div>
